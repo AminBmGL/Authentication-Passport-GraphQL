eb62a4754c9c3f1b7b5070c97b6cc2c187c671e2
Amin Bm <medaminbenmbarek@gmail.com>
2019-09-29 14:13:27 +0100

    Adding server setup and dependencies

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportConfig = require('./services/auth');
+const MongoStore = require('connect-mongo')(session);
+const schema = require('./schema/schema');
+
+// Create a new Express application
+const app = express();
+
+const MONGO_URI = '';
+
+// Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
+mongoose.Promise = global.Promise;
+
+// Connect to the mongoDB instance and log a message
+// on success or failure
+mongoose.connect(MONGO_URI);
+mongoose.connection
+    .once('open', () => console.log('Connected to MongoLab instance.'))
+    .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'aaabbbccc',
+  store: new MongoStore({
+    url: MONGO_URI,
+    autoReconnect: true
+  })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+
+app.listen(4000, () => {
+  console.log('Listening');
+});
